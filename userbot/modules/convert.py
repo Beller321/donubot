0b3a7b86732f93644c3083dee2443002f5a29ab1
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-25 09:29:20 -0800

    Update convert.py

--- userbot/modules/convert.py
+++ userbot/modules/convert.py
@@ -16,10 +16,10 @@ from PIL import Image
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, edit_or_reply, poci_cmd, runcmd
+from userbot.utils import edit_delete, edit_or_reply, cilik_cmd, runcmd
 
 
-@poci_cmd(pattern="convert ?(foto|audio|gif|voice|photo|mp3)? ?(.*)")
+@cilik_cmd(pattern="convert ?(foto|audio|gif|voice|photo|mp3)? ?(.*)")
 async def cevir(event):
     botman = event.pattern_match.group(1)
     try:
@@ -125,7 +125,7 @@ async def cevir(event):
         return
 
 
-@poci_cmd(pattern="makevoice$")
+@cilik_cmd(pattern="makevoice$")
 async def makevoice(event):
     if not event.reply_to:
         return await edit_delete(event, "**Mohon Balas Ke Audio atau video**")
