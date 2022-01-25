7f9b88d68e5bf432089de41d2ebcda13810da4c5
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-25 10:08:42 -0800

    Update nsfwdetect.py

--- userbot/modules/nsfwdetect.py
+++ userbot/modules/nsfwdetect.py
@@ -1,18 +1,18 @@
 # Copyright (C) 2020  @deleteduser420 <https://github.com/code-rgb>
 # ported by @mrismanaziz
 # FROM Man-Userbot
-# Recode by @Pocongonlen
+# Recode by @greyyvbss
 
 import os
 
 import requests
 
 from userbot import CMD_HELP, DEEP_AI, bot
-from userbot.events import poci_cmd
+from userbot.events import cilik_cmd
 from userbot.utils import edit_delete, edit_or_reply
 
 
-@bot.on(poci_cmd(outgoing=True, pattern=r"detect$"))
+@bot.on(cilik_cmd(outgoing=True, pattern=r"detect$"))
 async def detect(event):
     if DEEP_AI is None:
         return await edit_delete(
