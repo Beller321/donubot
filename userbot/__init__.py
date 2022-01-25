03ea0e0d4ef448855175f9e72f976b5d407f23d7
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-25 07:58:07 -0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -159,7 +159,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/poocong/PocongUserbot.git"
+    "UPSTREAM_REPO_URL", "https://github.com/grey423/CilikUserbot.git"
 )
 
 # SQL Database URI
