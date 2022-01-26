630b65c875ac8d1d13cf59e170af66c62b9cb6d4
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-26 12:40:00 +0700

    Update funmemes.py

--- userbot/modules/funmemes.py
+++ userbot/modules/funmemes.py
@@ -63,7 +63,7 @@ async def _(event):
         deq.rotate(1)
 
 
-@bot.on(poci_cmd(pattern=r"bombs", outgoing=True))
+@bot.on(cilik_cmd(pattern=r"bombs", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -469,7 +469,7 @@ async def _(event):
         await event.edit("EVERyBOdy wAs GanGeSTar UNtIL I ArRivEd 😎😎😎")
 
 
-@bot.on(poci_cmd(pattern=r"(.*)", outgoing=True))
+@bot.on(cilik_cmd(pattern=r"(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
