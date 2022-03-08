f3a7a35be18618f28091a09ecd5591e87bb60262
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-08 02:25:30 -0800

    Update utils.py

--- userbot/utils/utils.py
+++ userbot/utils/utils.py
@@ -1,232 +1,215 @@
-# Credits: @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
-# recode By @greyyvbss
+"""
+This module updates the userbot based on upstream revision
+"""
 
-import asyncio
-import importlib
-import logging
 import sys
-from pathlib import Path
-from random import randint
+from base64 import b64decode
+from os import environ, execle, remove
 
-import heroku3
-from telethon.tl.functions.contacts import UnblockRequest
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from userbot import (
-    BOT_TOKEN,
-    BOTLOG_CHATID,
-    CMD_HELP,
-    HEROKU_API_KEY,
-    HEROKU_APP_NAME,
-    LOGS,
-    bot,
-)
-
-heroku_api = "https://api.heroku.com"
-if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
-    Heroku = heroku3.from_key(HEROKU_API_KEY)
-    app = Heroku.app(HEROKU_APP_NAME)
-    heroku_var = app.config()
-else:
-    app = None
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, HEROKU_API_KEY, HEROKU_APP_NAME
+from userbot.events import register
+from userbot.utils import edit_delete, edit_or_reply, cilik_cmd
 
 
-async def autobot():
-    if BOT_TOKEN:
-        return
-    await bot.start()
-    await bot.send_message(
-        BOTLOG_CHATID, "**SEDANG MEMBUAT BOT TELEGRAM UNTUK ANDA DI @BotFather**"
+async def gen_chlog(repo, diff):
+    d_form = "%d/%m/%y"
+    return "".join(
+        f"• [{c.committed_datetime.strftime(d_form)}]: {c.summary} <{c.author}>\n"
+        for c in repo.iter_commits(diff)
     )
-    who = await bot.get_me()
-    name = who.first_name + " Assistant Bot"
-    if who.username:
-        username = who.username + "_ubot"
+
+
+async def print_changelogs(xx, ac_br, changelog):
+    changelog_str = (
+        f"**✥ Tersedia Pembaruan Untuk [{ac_br}] :\n\n✥ Pembaruan:**\n`{changelog}`"
+    )
+    if len(changelog_str) > 4096:
+        await edit_or_reply(xx, "**Changelog terlalu besar, dikirim sebagai file.**")
+        with open("output.txt", "w+") as file:
+            file.write(changelog_str)
+        await xx.client.send_file(xx.chat_id, "output.txt")
+        remove("output.txt")
     else:
-        username = "cilik" + (str(who.id))[5:] + "ubot"
-    bf = "@BotFather"
-    await bot(UnblockRequest(bf))
-    await bot.send_message(bf, "/cancel")
-    await asyncio.sleep(1)
-    await bot.send_message(bf, "/start")
-    await asyncio.sleep(1)
-    await bot.send_message(bf, "/newbot")
-    await asyncio.sleep(1)
-    isdone = (await bot.get_messages(bf, limit=1))[0].text
-    if isdone.startswith("That I cannot do."):
-        LOGS.info(
-            "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
-        )
-        sys.exit(1)
-    await bot.send_message(bf, name)
-    await asyncio.sleep(1)
-    isdone = (await bot.get_messages(bf, limit=1))[0].text
-    if not isdone.startswith("Good."):
-        await bot.send_message(bf, "My Assistant Bot")
-        await asyncio.sleep(1)
-        isdone = (await bot.get_messages(bf, limit=1))[0].text
-        if not isdone.startswith("Good."):
-            LOGS.info(
-                "Silakan buat Bot dari @BotFather dan tambahkan tokennya di var BOT_TOKEN"
+        await xx.client.send_message(xx.chat_id, changelog_str)
+    return True
+
+
+async def deploy(xx, repo, ups_rem, ac_br, txt):
+    if HEROKU_API_KEY is not None:
+        import heroku3
+
+        heroku = heroku3.from_key(HEROKU_API_KEY)
+        heroku_app = None
+        heroku_applications = heroku.apps()
+        if HEROKU_APP_NAME is None:
+            await edit_or_reply(
+                xx,
+                "**[HEROKU]: Harap Tambahkan Variabel** `HEROKU_APP_NAME` "
+                " **untuk deploy perubahan terbaru dari Userbot.**",
             )
-            sys.exit(1)
-    await bot.send_message(bf, username)
-    await asyncio.sleep(1)
-    isdone = (await bot.get_messages(bf, limit=1))[0].text
-    await bot.send_read_acknowledge("botfather")
-    if isdone.startswith("Sorry,"):
-        ran = randint(1, 100)
-        username = "cilik" + (str(who.id))[6:] + str(ran) + "ubot"
-        await bot.send_message(bf, username)
-        await asyncio.sleep(1)
-        nowdone = (await bot.get_messages(bf, limit=1))[0].text
-        if nowdone.startswith("Done!"):
-            token = nowdone.split("`")[1]
-            await bot.send_message(bf, "/setinline")
-            await asyncio.sleep(1)
-            await bot.send_message(bf, f"@{username}")
-            await asyncio.sleep(1)
-            await bot.send_message(bf, "Search")
-            await asyncio.sleep(3)
-            await bot.send_message(bf, "/setuserpic")
-            await asyncio.sleep(1)
-            await bot.send_message(bf, f"@{username}")
-            await asyncio.sleep(1)
-            await bot.send_file(bf, "userbot/resources/logo.jpg")
-            await asyncio.sleep(3)
-            await bot.send_message(bf, "/setabouttext")
-            await asyncio.sleep(1)
-            await bot.send_message(bf, f"@{username}")
-            await asyncio.sleep(1)
-            await bot.send_message(bf, f"Managed With ☕️ By {who.first_name}")
-            await asyncio.sleep(3)
-            await bot.send_message(bf, "/setdescription")
-            await asyncio.sleep(1)
-            await bot.send_message(bf, f"@{username}")
-            await asyncio.sleep(1)
-            await bot.send_message(
-                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @Lunatic0de ✨"
+            repo.__del__()
+            return
+        for app in heroku_applications:
+            if app.name == HEROKU_APP_NAME:
+                heroku_app = app
+                break
+        if heroku_app is None:
+            await edit_or_reply(
+                xx,
+                f"{txt}\n"
+                "**Kredensial Heroku tidak valid untuk deploy Cilik-Userbot dyno.**",
             )
-            await bot.send_message(
-                BOTLOG_CHATID,
-                f"**BERHASIL MEMBUAT BOT TELEGRAM DENGAN USERNAME @{username}**",
-            )
-            await bot.send_message(
-                BOTLOG_CHATID,
-                "**Tunggu Sebentar, Sedang MeRestart Heroku untuk Menerapkan Perubahan.**",
-            )
-            heroku_var["BOT_TOKEN"] = token
-            heroku_var["BOT_USERNAME"] = f"@{username}"
-        else:
-            LOGS.info(
-                "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
-            )
-            sys.exit(1)
-    elif isdone.startswith("Done!"):
-        token = isdone.split("`")[1]
-        await bot.send_message(bf, "/setinline")
-        await asyncio.sleep(1)
-        await bot.send_message(bf, f"@{username}")
-        await asyncio.sleep(1)
-        await bot.send_message(bf, "Search")
-        await asyncio.sleep(3)
-        await bot.send_message(bf, "/setuserpic")
-        await asyncio.sleep(1)
-        await bot.send_message(bf, f"@{username}")
-        await asyncio.sleep(1)
-        await bot.send_file(bf, "userbot/resources/logo.jpg")
-        await asyncio.sleep(3)
-        await bot.send_message(bf, "/setabouttext")
-        await asyncio.sleep(1)
-        await bot.send_message(bf, f"@{username}")
-        await asyncio.sleep(1)
-        await bot.send_message(bf, f"Managed With ☕️ By {who.first_name}")
-        await asyncio.sleep(3)
-        await bot.send_message(bf, "/setdescription")
-        await asyncio.sleep(1)
-        await bot.send_message(bf, f"@{username}")
-        await asyncio.sleep(1)
-        await bot.send_message(
-            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @CilikProject ✨"
-        )
-        await bot.send_message(
-            BOTLOG_CHATID,
-            f"**BERHASIL MEMBUAT BOT TELEGRAM DENGAN USERNAME @{username}**",
-        )
-        await bot.send_message(
-            BOTLOG_CHATID,
-            "**Tunggu Sebentar, Sedang MeRestart Heroku untuk Menerapkan Perubahan.**",
-        )
-        heroku_var["BOT_TOKEN"] = token
-        heroku_var["BOT_USERNAME"] = f"@{username}"
-    else:
-        LOGS.info(
-            "Silakan Hapus Beberapa Bot Telegram Anda di @Botfather atau Set Var BOT_TOKEN dengan token bot"
-        )
-        sys.exit(1)
-
-
-def load_module(shortname):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        path = Path(f"userbot/modules/{shortname}.py")
-        name = "userbot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        LOGS.info("Successfully imported " + shortname)
-    else:
-
-        path = Path(f"userbot/modules/{shortname}.py")
-        name = "userbot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.bot = bot
-        mod.LOGS = LOGS
-        mod.CMD_HELP = CMD_HELP
-        mod.logger = logging.getLogger(shortname)
-        spec.loader.exec_module(mod)
-        # for imports
-        sys.modules["userbot.modules." + shortname] = mod
-        LOGS.info("Successfully imported " + shortname)
-
-
-def start_assistant(shortname):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        path = Path(f"userbot/modules/assistant/{shortname}.py")
-        name = "userbot.modules.assistant.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        LOGS.info("Starting Your Assistant Bot.")
-        LOGS.info("Assistant Sucessfully imported " + shortname)
-    else:
-        path = Path(f"userbot/modules/assistant/{shortname}.py")
-        name = "userbot.modules.assistant.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.tgbot = bot.tgbot
-        spec.loader.exec_module(mod)
-        sys.modules["userbot.modules.assistant" + shortname] = mod
-        LOGS.info("Assistant Successfully imported" + shortname)
-
-
-def remove_plugin(shortname):
-    try:
+            return repo.__del__()
         try:
-            for i in CMD_HELP[shortname]:
-                bot.remove_event_handler(i)
-            del CMD_HELP[shortname]
+            from userbot.modules.sql_helper.globals import addgvar, delgvar
 
-        except BaseException:
-            name = f"userbot.modules.{shortname}"
+            delgvar("restartstatus")
+            addgvar("restartstatus", f"{xx.chat_id}\n{xx.id}")
+        except AttributeError:
+            pass
+        ups_rem.fetch(ac_br)
+        repo.git.reset("--hard", "FETCH_HEAD")
+        heroku_git_url = heroku_app.git_url.replace(
+            "https://", "https://api:" + HEROKU_API_KEY + "@"
+        )
+        if "heroku" in repo.remotes:
+            remote = repo.remote("heroku")
+            remote.set_url(heroku_git_url)
+        else:
+            remote = repo.create_remote("heroku", heroku_git_url)
+        try:
+            remote.push(refspec="HEAD:refs/heads/master", force=True)
+        except Exception as error:
+            await edit_or_reply(xx, f"{txt}\n**Terjadi Kesalahan Di Log:**\n`{error}`")
+            return repo.__del__()
+        build = heroku_app.builds(order_by="created_at", sort="desc")[0]
+        if build.status == "failed":
+            await edit_delete(
+                xx, "**Build Gagal!** Dibatalkan karena ada beberapa error.`"
+            )
+        await edit_or_reply(
+            xx, "`Cilik-Userbot Berhasil Di Deploy! Userbot bisa di gunakan kembali.`"
+        )
 
-            for i in reversed(range(len(bot._event_builders))):
-                ev, cb = bot._event_builders[i]
-                if cb.__module__ == name:
-                    del bot._event_builders[i]
+    else:
+        return await edit_delete(
+            xx, "**[HEROKU]: Harap Tambahkan Variabel** `HEROKU_API_KEY`"
+        )
+
+
+async def update(xx, repo, ups_rem, ac_br):
+    try:
+        ups_rem.pull(ac_br)
+    except GitCommandError:
+        repo.git.reset("--hard", "FETCH_HEAD")
+    await edit_or_reply(
+        xx, "`Cilik-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`"
+    )
+
+    try:
+        from userbot.modules.sql_helper.globals import addgvar, delgvar
+
+        delgvar("restartstatus")
+        addgvar("restartstatus", f"{xx.chat_id}\n{xx.id}")
+    except AttributeError:
+        pass
+
+    # Spin a new instance of bot
+    args = [sys.executable, "-m", "userbot"]
+    execle(sys.executable, *args, environ)
+
+
+@cilik_cmd(pattern="update( now| deploy|$)")
+@register(pattern=r"^\.cupdate( now| deploy|$)", sudo=True)
+async def upstream(event):
+    "For .update command, check if the bot is up to date, update if specified"
+    xx = await edit_or_reply(event, "`Mengecek Pembaruan, Tunggu Sebentar...`")
+    conf = event.pattern_match.group(1).strip()
+    off_repo = b64decode(
+        "aHR0cHM6Ly9naXRodWIuY29tL2dyZXk0MjMvQ2lsaWtVc2VyYm90"
+    ).decode("utf-8")
+    force_update = False
+    try:
+        txt = (
+            "**Pembaruan Tidak Dapat Di Lanjutkan Karna "
+            + "Terjadi Beberapa ERROR**\n\n**LOGTRACE:**\n"
+        )
+        repo = Repo()
+    except NoSuchPathError as error:
+        await xx.edit(f"{txt}\n**Directory** `{error}` **Tidak Dapat Di Temukan.**")
+        return repo.__del__()
+    except GitCommandError as error:
+        await xx.edit(f"{txt}\n**Kegagalan awal!** `{error}`")
+        return repo.__del__()
+    except InvalidGitRepositoryError as error:
+        if conf is None:
+            return await xx.edit(
+                f"**Sayangnya, Directory {error} Tampaknya Bukan Dari Repo. Tapi Kita Bisa Memperbarui Paksa Userbot Menggunakan** `{cmd}update deploy`"
+            )
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        force_update = True
+        repo.create_head("master", origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
+
+    ac_br = repo.active_branch.name
+    try:
+        repo.create_remote("upstream", off_repo)
     except BaseException:
-        raise ValueError
+        pass
+
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(ac_br)
+
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
+    if conf == "deploy":
+        await xx.edit("`[HEROKU]: Update Deploy Cilik-Userbot Sedang Dalam Proses...`")
+        await deploy(xx, repo, ups_rem, ac_br, txt)
+        return
+
+    if changelog == "" and not force_update:
+        await edit_delete(xx, "**✥ Cilik-Userbot Sudah Versi Terbaru ✥**")
+        return repo.__del__()
+
+    if conf == "" and not force_update:
+        await print_changelogs(xx, ac_br, changelog)
+        await xx.delete()
+        return await event.respond(
+            f"**Ketik** `{cmd}update deploy` **untuk Mengupdate Userbot.**"
+        )
+
+    if force_update:
+        await xx.edit("**Sinkronisasi Paksa Ke Kode Userbot Terbaru, Harap Tunggu...**")
+
+    if conf == "now":
+        for commit in changelog.splitlines():
+            if (
+                commit.startswith("- [NQ]")
+                and HEROKU_APP_NAME is not None
+                and HEROKU_API_KEY is not None
+            ):
+                return await xx.edit(
+                    f"**Quick update telah dinonaktifkan untuk pembaruan ini Gunakan** `{cmd}update deploy` **sebagai gantinya.**"
+                )
+        await xx.edit("**Perfoming a quick update, please wait...**")
+        await update(xx, repo, ups_rem, ac_br)
+
+    return
+
+
+CMD_HELP.update(
+    {
+        "update": f"**Plugin : **`update`\
+        \n\n  •  **Syntax :** `{cmd}update`\
+        \n  •  **Function : **Untuk Melihat Pembaruan Terbaru Cilik-Userbot.\
+        \n\n  •  **Syntax :** `{cmd}update deploy`\
+        \n  •  **Function : **Untuk MengUpdate Fitur Terbaru Dari Cilik-Userbot.\
+    "
+    }
+)
