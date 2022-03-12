ffbf6f14b7f634826042a88e1a50b9ad12035b75
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-12 17:47:51 +0700

    Update tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -12,7 +12,7 @@ import re
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
-from userbot.events import cilik_cmd
+from userbot.utils import cilik_cmd
 
 usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
 nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
@@ -25,7 +25,7 @@ class FlagContainer:
     is_active = False
 
 
-@bot.on(cilik_cmd(outgoing=True, pattern=r"mention(?: |$)(.*)"))
+@cilik_cmd(pattern="mention(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -38,7 +38,7 @@ async def _(event):
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
-@bot.on(cilik_cmd(outgoing=True, pattern=r"emojitag(?: |$)(.*)"))
+@cilik_cmd(pattern="emojitag(?: |$)(.*)")
 async def _(event):
     if event.fwd_from or FlagContainer.is_active:
         return
@@ -81,7 +81,7 @@ async def _(event):
         FlagContainer.is_active = False
 
 
-@bot.on(cilik_cmd(outgoing=True, pattern=r"all(?: |$)(.*)"))
+@cilik_cmd(pattern="all(?: |$)(.*)")
 async def _(event):
     if event.fwd_from or FlagContainer.is_active:
         return
