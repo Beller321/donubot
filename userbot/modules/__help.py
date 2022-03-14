d63d73cfd5e6715add131684514d51f7edf20090
grey423 <geryandgrey@gmail.com>
2022-03-14 10:52:32 +0000

    CilikUserbot: auto fixes
    Signed-off-by: grey423 <geryandgrey@gmail.com>

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -18,9 +18,8 @@
 #    From Flicks-Userbot
 #    <t.me/TheFlicksUserbot>
 
-import os
 
-from userbot import ALIVE_NAME, BOT_USERNAME, CMD_HELP, CMD_HANDLER, CMD_LIST, bot, tgbot
+from userbot import BOT_USERNAME, CMD_HELP, bot
 from userbot.utils import cilik_cmd
 
 user = bot.get_me()
