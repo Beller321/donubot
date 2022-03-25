93c8c4c26d361aac7b3dbbd355d4c9990e31cd55
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-25 01:19:50 -0700

    try

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -387,7 +387,7 @@ else:
 
 
 def paginate_help(page_number, loaded_modules, prefix):
-    number_of_rows = 4
+    number_of_rows = 5
     number_of_cols = 2
     global looters
     looters = page_number
