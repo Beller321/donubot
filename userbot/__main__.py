68de6b2700e4a794eb951f3eb2d811d33864af47
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-25 13:39:45 -0700

    last

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -15,8 +15,9 @@ from importlib import import_module
 
 import requests
 from pytgcalls import idle
-from telethon.tl.functions.channels import InviteToChannelRequest
-from userbot import BOTLOG_CHATID, BOT_TOKEN, BOT_VER
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
+from userbot import BOT_TOKEN, BOT_USERNAME, BOT_VER, BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
 from userbot import LOGS, bot, call_py
 from userbot.modules import ALL_MODULES
 from userbot.utils import autopilot, autobot, checking
@@ -40,41 +41,32 @@ except Exception as e:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
+LOGS.info(
+    f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/CilikSupport"
+)
+
+LOGS.info(f"🔥 Cilik-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+
 if not BOTLOG_CHATID:
     LOGS.info(
         "BOTLOG_CHATID Vars tidak terisi, Memulai Membuat Grup Otomatis..."
     )
     bot.loop.run_until_complete(autopilot())
 
-LOGS.info(
-    f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/CilikSupport"
-)
-
-LOGS.info(f"Cilik-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]"    
-
-          
 async def cilik_userbot_on():
     try:
         if BOTLOG_CHATID != 0:
             await bot.send_message(
                 BOTLOG_CHATID,
-                f"🔥 **Cilik-Userbot Berhasil Di Aktifkan** 🔥\n━━━━━━━━━━\n➠ **Userbot Version -** `{BOT_VER}@{branch}`\n➠ **Ketik** `{cmd}ping` **untuk Mengecheck Bot**\n━━━━━━━━━━",
+                f"🔥 **Cilik-Userbot Berhasil Di Aktifkan** 🔥\n━━\n➠ **Userbot Version -** `{BOT_VER}`\n➠ **Ketik** `{cmd}alive` **Untuk Mengecek Bot**\n━━",
             )
     except Exception as e:
         LOGS.info(str(e))
-    try:
-        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
+    
 
-    
-    
 bot.loop.run_until_complete(checking())
 bot.loop.run_until_complete(cilik_userbot_on())
 if not BOT_TOKEN:
-    LOGS.info(
-        "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
-    )
     bot.loop.run_until_complete(autobot())
 idle()
 if len(sys.argv) not in (1, 3, 4):
