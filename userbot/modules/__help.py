d2becec70a276fe41f4364e52646be6009a8327f
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-30 18:10:02 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -40,7 +40,7 @@ async def cmd_list(event):
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@CilikSupport"
+                BOT_USERNAME, "@CilikUserbot"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
