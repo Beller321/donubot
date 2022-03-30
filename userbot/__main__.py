f67f3b05c582d0b3981c9ea51b964e42fdc8f789
Grey Project <95908917+grey423@users.noreply.github.com>
2022-03-30 00:39:03 -0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -2,16 +2,14 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-
+#
 """ Userbot start point """
 
 import sys
 from importlib import import_module
 
 import requests
-from telethon.tl.functions.channels import JoinChannelRequest as grey
-from telethon.tl.functions.channels import InviteToChannelRequest as cilik
-from pytgcalls import idle
+from telethon.tl.functions.channels import InviteToChannelRequest as Cilik
 from userbot import (
     BOTLOG_CHATID,
     BOT_USERNAME,
@@ -19,23 +17,24 @@ from userbot import (
     BOT_VER,
     ALIVE_LOGO,
     LOGS,
+    cilikblacklist,
     bot,
     call_py,
 )
-from userbot import LOGS, bot, call_py
+from userbot import CMD_HANDLER as cmd
 from userbot.modules import ALL_MODULES
-from userbot.utils import autopilot, autobot, checking
+from userbot.utils import autobot, autopilot
 
 try:
     bot.start()
     call_py.start()
     user = bot.get_me()
-    blacklistcilik = requests.get(
+    cilikblacklist = requests.get(
         "https://raw.githubusercontent.com/grey423/Reforestation/master/cilikblacklist.json"
     ).json()
-    if user.id in blacklistcilik:
+    if user.id in cilikblacklist:
         LOGS.warning(
-            "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOTnya GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nCredits: @greyyvbss"
+            "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOTnya GUA MATIIN NAJIS BANGET DIPAKE ORANG KEK LU.\nCredits: @greyyvbss"
         )
         sys.exit(1)
 except Exception as e:
@@ -55,39 +54,26 @@ LOGS.info(
     f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/CilikSupport")
 LOGS.info(
     f"🔥 Cilik-Userbot 🔥 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
-    
-async def cilik_userbot_on():
+
+
+async def cilik_bot_run():
     try:
         if BOTLOG_CHATID != 0:
-            await bot.send_message(
-                BOTLOG_CHATID,
-                ALIVE_LOGO,
-                f"🔥 **Cilik Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 1.1 @Cilik-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecheck Bot**\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @CilikProject ",
-            )
+            await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"🔥 **Cilik Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 1.1 @Cilik-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecheck Bot**\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @CilikProject ")
     except Exception as e:
         LOGS.info(str(e))
     try:
-        await bot(cilik(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass          
-    try:
-        await bot(grey("@CilikSupport"))
-    except BaseException:
-        pass
-    try:
-        await bot(grey("@CilikProject"))
+        await bot(Cilik(int(BOTLOG_CHATID), [BOT_USERNAME]))
     except BaseException:
         pass
 
-bot.loop.run_until_complete(checking())
-bot.loop.run_until_complete(cilik_userbot_on())
+bot.loop.run_until_complete(cilik_bot_run())
 if not BOT_TOKEN:
     LOGS.info(
         "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
     )
     bot.loop.run_until_complete(autobot())
 
-idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
