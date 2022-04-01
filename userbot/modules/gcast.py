0cd7f5ffae4cc938a387be0896ec22e9eb83167b
Grey Project <95908917+grey423@users.noreply.github.com>
2022-04-01 01:31:15 -0700

    y

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -7,14 +7,19 @@
 # FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
 # t.me/SharingUserbot & t.me/Lunatic0de
 # recode by @greyyvbss
+
 import asyncio
+import os
 
+import heroku3
 from requests import get
-from telethon.errors.rpcerrorlist import FloodWaitError
+from telethon.errors import FloodWaitError
 
+from userbot import BLACKLIST_GCAST
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, DEVS
+from userbot import CMD_HELP, DEVS, HEROKU_API_KEY, HEROKU_APP_NAME
 from userbot.utils import edit_delete, edit_or_reply, cilik_cmd
+
 
 while 0 < 6:
     _GCAST_BLACKLIST = get(
@@ -31,10 +36,14 @@ while 0 < 6:
 del _GCAST_BLACKLIST
 
 
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+blchat = os.environ.get("BLACKLIST_GCAST") or ""
+
+
 @cilik_cmd(pattern="gcast(?: |$)(.*)")
 async def gcast(event):
-    xx = event.pattern_match.group(1)
-    if xx:
+    if xx := event.pattern_match.group(1):
         msg = xx
     elif event.is_reply:
         msg = await event.get_reply_message()
@@ -46,7 +55,7 @@ async def gcast(event):
     async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
-            if chat not in GCAST_BLACKLIST:
+            if chat not in GCAST_BLACKLIST and chat not in BLACKLIST_GCAST:
                 try:
                     await event.client.send_message(chat, msg)
                     await asyncio.sleep(0.1)
@@ -64,8 +73,7 @@ async def gcast(event):
 
 @cilik_cmd(pattern="gucast(?: |$)(.*)")
 async def gucast(event):
-    xx = event.pattern_match.group(1)
-    if xx:
+    if xx := event.pattern_match.group(1):
         msg = xx
     elif event.is_reply:
         msg = await event.get_reply_message()
@@ -93,11 +101,92 @@ async def gucast(event):
     )
 
 
+@cilik_cmd(pattern="blchat$")
+async def sudo(event):
+    blacklistgc = "True" if BLACKLIST_GCAST else "False"
+    blc = blchat
+    list = blc.replace(" ", "\n» ")
+    if blacklistgc == "True":
+        await edit_or_reply(
+            event,
+            f"🔮 **Blacklist GCAST:** `Enabled`\n\n📚 **Blacklist Group:**\n» {list}\n\nKetik `.addblacklist` di grup yang ingin anda tambahkan ke daftar blacklist gcast.",
+        )
+    else:
+        await edit_delete(event, "🔮 **Blacklist GCAST:** `Disabled`")
+
+
+@cilik_cmd(pattern="addblacklist(?:\\s|$)([\\s\\S]*)")
+async def add(event):
+    xxnx = await edit_or_reply(event, "`Processing...`")
+    var = "BLACKLIST_GCAST"
+    gc = event.chat_id
+    if HEROKU_APP_NAME is not None:
+        app = Heroku.app(HEROKU_APP_NAME)
+    else:
+        await edit_delete(
+            xxnx,
+            "**Silahkan Tambahkan Var** `HEROKU_APP_NAME` **untuk menambahkan blacklist**",
+        )
+        return
+    heroku_Config = app.config()
+    if event is None:
+        return
+    blgc = f"{BLACKLIST_GCAST} {gc}"
+    blacklistgrup = (
+        blgc.replace("{", "")
+        .replace("}", "")
+        .replace(",", "")
+        .replace("[", "")
+        .replace("]", "")
+        .replace("set() ", "")
+    )
+    await xxnx.edit(
+        f"**Berhasil Menambahkan** `{gc}` **ke daftar blacklist gcast.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
+    )
+    heroku_Config[var] = blacklistgrup
+
+
+@cilik_cmd(pattern="delblacklist(?:\\s|$)([\\s\\S]*)")
+async def _(event):
+    xxx = await edit_or_reply(event, "`Processing...`")
+    gc = event.chat_id
+    if HEROKU_APP_NAME is not None:
+        app = Heroku.app(HEROKU_APP_NAME)
+    else:
+        await edit_delete(
+            xxx,
+            "**Silahkan Tambahkan Var** `HEROKU_APP_NAME` **untuk menghapus blacklist**",
+        )
+        return
+    heroku_Config = app.config()
+    if event is None:
+        return
+    gett = str(gc)
+    if gett in blchat:
+        blacklistgrup = blchat.replace(gett, "")
+        await xxx.edit(
+            f"**Berhasil Menghapus** `{gc}` **dari daftar blacklist gcast.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
+        )
+        var = "BLACKLIST_GCAST"
+        heroku_Config[var] = blacklistgrup
+    else:
+        await edit_delete(
+            xxx, "**Grup ini tidak ada dalam daftar blacklist gcast.**", 45
+        )
+
+
 CMD_HELP.update(
     {
-        "gcast": f"**Plugin : **`gcast`\
-        \n\n  •  **Syntax :** `{cmd}gcast` <text/reply media>\
-        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Grup yang kamu masuk. (Bisa Mengirim Media/Sticker)\
+        "gcast": f"**➢ Plugin : **`gcast`\
+        \n\n ┌✪ **Syntax :** `{cmd}gcast` <text/reply media>\
+        \n └✪ **Function : **Mengirim Global Broadcast pesan ke Seluruh Grup yang kamu masuk. (Bisa Mengirim Media/Sticker)\
+        \n\n ┌✪ **Syntax :** `{cmd}blchat`\
+        \n └✪ **Function : **Untuk Mengecek informasi daftar blacklist gcast.\
+        \n\n ┌✪ **Syntax :** `{cmd}addblacklist`\
+        \n └✪ **Function : **Untuk Menambahkan grup tersebut ke blacklist gcast.\
+        \n\n ┌✪ **Syntax :** `{cmd}delblacklist`\
+        \n └✪ **Function : **Untuk Menghapus grup tersebut dari blacklist gcast.\
+        \n ➠ **Note : **Ketik perintah** `{cmd}addblacklist` **dan** `{cmd}delblacklist` **di grup yang kamu Blacklist.\
     "
     }
 )
@@ -106,8 +195,8 @@ CMD_HELP.update(
 CMD_HELP.update(
     {
         "gucast": f"**Plugin : **`gucast`\
-        \n\n  •  **Syntax :** `{cmd}gucast` <text/reply media>\
-        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)\
+        \n\n ┌✪ **Syntax :** `{cmd}gucast` <text/reply media>\
+        \n └✪ **Function : **Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)\
     "
     }
 )
