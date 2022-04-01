3cd553bbc39073e9dd6f13980a8cc7f90501d04a
Grey Project <95908917+grey423@users.noreply.github.com>
2022-04-01 01:33:15 -0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -116,6 +116,7 @@ DEVS = (
   
 SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "1784606556").split()}
 BL_CHAT = {int(x) for x in os.environ.get("BL_CHAT", "").split()}
+BLACKLIST_GCAST = {int(x) for x in os.environ.get("BLACKLIST_GCAST", "").split()}
 
 # For Blacklist Group Support
 BLACKLIST_CHAT = os.environ.get("BLACKLIST_CHAT", None)
