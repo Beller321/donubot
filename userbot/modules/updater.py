d8198614ef2c2e5313b49cc38def6de42dc54fa6
Grey <95908917+grey423@users.noreply.github.com>
2022-04-11 18:57:06 -0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -60,7 +60,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await edit_or_reply(event,
                                 "`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `"
-                                " untuk dapat deploy perubahan terbaru dari ⚡xᴀ-ᴜsᴇʀʙᴏᴛ⚡.`"
+                                " untuk dapat deploy perubahan terbaru dari ⚡ Cilik-Userbot ⚡.`"
                                 )
             repo.__del__()
             return
@@ -70,7 +70,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                 break
         if heroku_app is None:
             await edit_delete(event,
-                              f"{txt}\n`Kredensial Heroku tidak valid untuk deploy Xa-Project dyno.`"
+                              f"{txt}\n`Kredensial Heroku tidak valid untuk deploy Cilik-Project dyno.`"
                               )
             return repo.__del__()
         await edit_or_reply(event,
@@ -98,12 +98,12 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
                               )
         else:
             await edit_delete(event,
-                              "`Xa-Userbot Berhasil DiUpdate✅,Restart Tunggu Sebentar`"
+                              "`⚡ Cilik-Userbot ⚡ Berhasil DiUpdate✅,Restart Tunggu Sebentar`"
                               )
 
         if BOTLOG:
             await event.client.send_message(
-                BOTLOG_CHATID, "#BOT \n" "`Xa-Userbot Berhasil Di Update`"
+                BOTLOG_CHATID, "#BOT \n" "`⚡ Cilik-Userbot ⚡ Berhasil Di Update`"
             )
 
     else:
@@ -119,9 +119,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    x = await edit_or_reply(event, "**⚡xᴀ-ᴜsᴇʀʙᴏᴛ⚡** `Berhasil Di Update!`")
+    x = await edit_or_reply(event, "**⚡ Cilik-Userbot ⚡** `Berhasil Di Update!`")
     await asyncio.sleep(1)
-    await x.edit("**⚡xᴀ-ᴜsᴇʀʙᴏᴛ⚡** `Di Restart....`")
+    await x.edit("**⚡ Cilik-Userbot ⚡** `Di Restart....`")
     await asyncio.sleep(1)
     await x.edit("`Mohon Menunggu Beberapa Detik.`")
     await asyncio.sleep(10)
@@ -129,7 +129,7 @@ async def update(event, repo, ups_rem, ac_br):
 
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, "#BOT \n" "**⚡xᴀ-ᴜsᴇʀʙᴏᴛ⚡ Telah Di Perbarui.**"
+            BOTLOG_CHATID, "#BOT \n" "**⚡ Cilik-Userbot ⚡ Telah Di Perbarui.**"
         )
         await asyncio.sleep(100)
         await x.delete()
@@ -195,7 +195,7 @@ async def upstream(event):
 
     if changelog == "" and force_update is False:
         await xx.edit(
-            f"\n⚡xᴀ-ᴜsᴇʀʙᴏᴛ⚡ Sudah Versi Terbaru || Tunggu Update Terbaru\n"
+            f"\n⚡ Cilik-Userbot ⚡ Sudah Versi Terbaru || Tunggu Update Terbaru\n"
         )
         await asyncio.sleep(15)
         await xx.delete()
