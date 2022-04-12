53a5c6fc89135d669c7192b8cebb5d7c00ec4eac
Grey <95908917+grey423@users.noreply.github.com>
2022-04-11 18:44:43 -0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -171,6 +171,8 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL", "https://github.com/grey423/CilikUserbot.git"
 )
+UPSTREAM_REPO_BRANCH = os.environ.get(
+    "UPSTREAM_REPO_BRANCH", "Cilik-Userbot")
 
 # SQL Database URI
 DB_URI = os.environ.get("DATABASE_URL", None)
